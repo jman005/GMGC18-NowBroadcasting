a3eb8fd77e3ec5ecfeb1e9b4049ea8ca996a7a50
Jman005 (Justin) <n/a>
2018-07-15 12:34:06 -0400

    test

---
+++ entities/weapons/nbdefault.lua
@@ -0,0 +1,16 @@
+--Don't mind me, I'm just the generic hand entity no. 32567. Carry on
+SWEP.Primary.ClipSize		= -1
+SWEP.Primary.DefaultClip	= -1
+SWEP.Primary.Automatic		= true
+SWEP.Primary.Ammo		= "none"
+
+SWEP.Secondary.ClipSize		= -1
+SWEP.Secondary.DefaultClip	= -1
+SWEP.Secondary.Automatic	= false
+SWEP.Secondary.Ammo		= "none"
+SWEP.ViewModel			= "models/weapons/v_hands.mdl"
+SWEP.WorldModel			= "models/weapons/w_pistol.mdl"
+
+function SWEP:PrimaryAttack()
+	
+end
